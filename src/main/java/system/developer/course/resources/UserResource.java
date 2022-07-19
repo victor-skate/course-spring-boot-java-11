4f498fc8e54adffbca8630bb0ed7686aea4e21bb
victor moraes <victorhardflipmoraes@outlook.com>
2022-07-19 00:56:48 -0400

    Order, ISO 8601 - PART 1

--- src/main/java/system/developer/course/resources/UserResource.java
+++ src/main/java/system/developer/course/resources/UserResource.java
@@ -1,8 +1,6 @@
 package system.developer.course.resources;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
